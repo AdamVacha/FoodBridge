be0ff189d4682de2ab45b9ece8331edcc503bcbb
jtorruellas22 <jtorruellas@ufl.edu>
2023-12-02 20:37:06 -0500

    Updated Shelter Finder Page
    This has been an ongoing project. Due to many issues with Git and Github as well as other technology failures, it is all in one commit

--- src/routes/pages/shelter finder/+page.svelte
+++ src/routes/pages/shelter finder/+page.svelte
@@ -1,50 +1,70 @@
 <script>
-	// import { createShelterSearch, handeler} from '$lib/shelters/shelterFinder';
-	// import type { PageData } from '../$types';
-	// import { onDestroy } from 'svelte';
+	import { operationStore, query, setClient } from '@urql/svelte';
+	import { writable } from 'svelte/store';
+	import client from 'C:/FoodBridge/FoodBridgeMain-main/src/client.js'
+	setClient(client);
 
-	// export let data: PageData;
+	const listShelters = operationStore(
+		`
+		query {
+			listShelters {
+				data {
+					_id 
+					name
+                    city
+					state
+					email
+				}
+			}
+		}
+		`
+	);
 
-	// type Shelter = {
-	// 	city: string,
-	// 	state: string,
-	// 	name: string,
-	// 	searchTerms: string
-	// }
+	query(listShelters);
+	
+	let name = "";
 
-	// const searchForShelter: Shelter = data.shelters.map((shelter: Shelter[]) => ({
-	// 	...shelter,
-	// 	searchTerms: `${shelter.city} ${shelter.state} ${shelter.name}`
-	// 	}));
-
-	// const searchShelter = createShelterSearch(searchForShelter);
-
-	// const unsubscribe = searchShelter.subscribe((model) => handeler(model));
-
-	// onDestroy(() => {
-	// 	unsubscribe();
-	// });
 </script>
-
-<div class="product-grid">
-	<!-- {#each $searchShelter as shelter}
-		<div class="shelter">
-			<h2>{shelter.name}</h2>
-			<p>{shelter.city}</p>
-			<p class="badge">{shelter.state}</p>
-		</div>
-	{/each} -->
-</div>
 
 <section class="main">
 	<section class="shelter-finder">
 		<h1>Help us bridge the gap</h1>
 		<div class="container">
 			<h2>Your journey begins here</h2>
-			<input type="search" placeholder="Search our partners..." />
+			<input class="text" bind:value={name} placeholder="Search our partners..." />
 		</div>
+		<h2>Results
+		</h2>
+
+		{#if $listShelters.fetching}
+		<p>Loading...</p>
+		{:else if $listShelters.error}
+		<p>Oh no... {$listShelters.error.message}</p>
+		{:else}
+			<ul>
+				
+				{#each $listShelters.data.listShelters.data as shelter}
+					{#if shelter.name.toUpperCase().includes(name.toUpperCase()) || name == "" 
+						|| shelter.city.toUpperCase().includes(name.toUpperCase()) 
+							|| shelter.state.toUpperCase().includes(name.toUpperCase())}
+						<h3>
+							{shelter.name} - {shelter.city}, {shelter.state}
+							<h4>
+								{shelter.email}
+								<button>Donate here</button>
+							</h4>
+							
+						</h3>
+						
+						
+					{/if}
+				{/each}
+			</ul>
+		{/if}
 	</section>
 </section>
+
+
 
 <style>
 	* {
@@ -53,7 +73,7 @@
 		font-family: 'Montserrat', sans-serif;
 	}
 
-	/* .main {
+	.main {
 		min-height: 100vh;
 		width: 100%;
 		background-position: center;
@@ -61,7 +81,8 @@
 		position: relative;
 		background-image: linear-gradient(rgba(170, 108, 84, 0.795), rgba(86, 73, 49, 0.702)),
 			url('/images/foodbridge.webp');
-	} */
+	}
+
 	.shelter-finder {
 		padding-left: 0%;
 		padding-right: 0%;
@@ -70,11 +91,40 @@
 	}
 
 	h1 {
-		font-size: 42px;
+		font-size: 70px;
 		color: rgb(255, 177, 99);
 		margin-bottom: 20px;
 		margin-top: 20px;
 	}
-	/* .search {
-	} */
+
+	h2 {
+		font-size: 42px;
+		color: rgb(255, 177, 99);
+		margin-bottom: 20px;
+		margin-top: 20px;
+		text-align: center;
+	}
+
+	h3 {
+		font-size: 42px;
+		color: rgb(255, 255, 254);
+		text-align: center;
+	}
+
+	h4 {
+		font-size: 35px;
+		color: rgb(255, 255, 254);
+		font-weight: normal;
+		text-align: center;
+	}
+
+	input {
+		width: 400px;
+		height: 40px;
+		text-align: center;
+		font-size: 30px;
+		box-shadow: 10px 10px 5px rgb(102, 73, 42);
+		border: none;
+		border-radius: 20px;
+	}
 </style>
